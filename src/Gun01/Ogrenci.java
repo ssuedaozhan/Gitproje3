7ac21bf517e986c5b6e93417c3cd685472b4a48c
ssuedaozhan <suedaozhan1998@gmail.com>
2023-01-27 14:58:17 +0200

    ikinci gorevimi yaptim

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -1,5 +1,10 @@
 package Gun01;
 
 public class Ogrenci {
-    //dsdfdffd
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+
+        }
+    }
 }
